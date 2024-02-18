91d304f5f6532b474d0cd215b58fbfc0b7df0af6
Rasmus Nordling <nordlingcareers@gmail.com>
2024-02-18 22:31:35 +0100

    New post for January 2024 (#34)

--- _config.ts
+++ _config.ts
@@ -1,7 +1,8 @@
 import lume from "lume/mod.ts";
-import blog from "https://denopkg.com/happystinson/vento-blog-theme@v0.0.1/mod.ts";
+import blog from "https://denopkg.com/happystinson/vento-blog-theme@v0.0.2-pre/mod.ts";
 
-const site = lume();
+const site = lume({});
+
 site.use(blog());
 
 export default site;
